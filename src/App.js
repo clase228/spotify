f75b25fef10f688c5b4eb46c19686fb4083f9af2
clase228 <polina.mumber@gmail.com>
2023-06-02 19:40:30 +0600

    lesson 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import mainNav from './mainNav';
+import mainCenterblock from './mainCenterblock';
+import mainSidebar from './mainSidebar';
+import bar from './bar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='container'>
+         <div className='main'>
+            {mainNav()}
+            {mainCenterblock()}
+            {mainSidebar()}
+            {bar()}
+         </div>
+      </div>
     </div>
   );
 }
+console.log(mainNav);
 
 export default App;
